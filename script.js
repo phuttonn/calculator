17ded0d6729f969afd48a10f07115bb85a431902
phuttonn <prestonhutton93@gmail.com>
2023-01-08 16:54:43 -0700

    Add basic arithmetic functions

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = (a,b) => {
+    return a + b;
+}
+
+const subtract = (a,b) => {
+    return a - b;
+}
+
+const mult = (a,b) => {
+    return a * b;
+}
+
+const divide = (a,b) => {
+    return a / b;
+}
